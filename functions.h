c267b51fcf849c596d5f662bd588003b6b838c3a
Arthur Assis <arthur120496@gmail.com>
2018-11-08 08:45:42 -0200

    adds waiting queue
    Co-authored-by: Arthur Assis <arthur120496@gmail.com>
    Co-authored-by: Andre Filho <andre.filho001@outlook.com>

--- functions.h
+++ functions.h
@@ -4,11 +4,37 @@
 #include<unistd.h> 
 #include<semaphore.h>
 
-int num_students,turn;
-sem_t assistent_sem,student_sem,turn_sem;
+int num_students,turn,*queue,queue_max_size,queue_size = 0,assistent_state;
+sem_t assistent_sem,student_sem,turn_sem[40];
+pthread_mutex_t queue_mutex,assistent_state_mutex;
 
 int random_number(int min,int max){
   return ((rand()%(max-min+1))+min);
+}
+
+void add_to_queue(int id){
+  printf("the student(%d) is sitting in chair number %d\n",id,queue_size);
+  queue[queue_size++] = id;
+
+}
+
+void remove_from_queue(){
+  turn = queue[0];
+  printf("the student(%d) is getting help\n",turn);
+  int i;
+  for(i = 0; i < queue_size; i++){
+    queue[i] = queue[i+1];
+    printf("the student(%d) is sitting in chair number %d\n",turn,i);
+  }
+  queue_size--;
+
+}
+
+void take_a_nap(){
+  pthread_mutex_lock(&assistent_state_mutex); 
+  assistent_state = 1;
+  printf("the assistent is sleeping\n");
+  pthread_mutex_unlock(&assistent_state_mutex); 
 }
 
 void program(int id){
@@ -22,15 +48,25 @@ void wait_for_student(){
   sem_wait(&student_sem);
 }
 
-void wait_for_assistent(int id){
-  printf("the student(%d) is waiting\n",id);
-  sem_wait(&assistent_sem);
-}
-
-void get_help(int id){
-  turn = id;
-  printf("the student(%d) is getting help\n",turn);
-  sem_post(&student_sem);
+int get_help(int id){
+  pthread_mutex_lock(&queue_mutex);
+  pthread_mutex_lock(&assistent_state_mutex); 
+  if(assistent_state){
+    turn = id;
+    printf("the student(%d) is getting help\n",turn);
+    sem_post(&student_sem);
+  }
+  else if(queue_size < queue_max_size){
+    add_to_queue(id);
+  }
+  else{
+    pthread_mutex_unlock(&assistent_state_mutex); 
+    pthread_mutex_unlock(&queue_mutex);
+    return 0;
+  }
+  pthread_mutex_unlock(&assistent_state_mutex); 
+  pthread_mutex_unlock(&queue_mutex);
+  return 1;
 }
 
 void help_student(){
@@ -41,37 +77,53 @@ void help_student(){
 }
 
 void relise_student(){
-  printf("the assistent is sleeping\n");
-  sem_post(&turn_sem);
-  sem_post(&assistent_sem);
+  sem_post(&turn_sem[turn-1]);
+  pthread_mutex_lock(&queue_mutex);
+  if(!queue_size){  
+    take_a_nap();
+    sem_post(&assistent_sem);
+  }
+  pthread_mutex_unlock(&queue_mutex);
 
 }
 
 void* assistent(void *params){
   sem_wait(&student_sem);
-  sem_wait(&turn_sem);
-  printf("the assistent is sleeping\n");
+  take_a_nap();
   int n = num_students *3;
   while(n){
-    wait_for_student();
-    printf("the assistent is awake\n");
+    if(assistent_state){
+      wait_for_student();
+      assistent_state = 0;
+      printf("the assistent is awake\n");
+    }
+    else{
+      pthread_mutex_lock(&queue_mutex);
+      remove_from_queue();
+      pthread_mutex_unlock(&queue_mutex);
+    }
     help_student();
-    n--;
     relise_student();
+    n--;
   }
   pthread_exit(0);
 }
 
 void* student(void *params){
   int *id = (int*) params;
+  int i = *id;
+  sem_wait(&turn_sem[i-1]);
   printf("the student(%d) was created\n",*id);
   int times_helped;
   for(times_helped =0; times_helped < 3; times_helped++){
     program(*id);
-    wait_for_assistent(*id);
-    get_help(*id);
-    sem_wait(&turn_sem);
+    if(get_help(*id)){
+      sem_wait(&turn_sem[i-1]);
+    }
+    else{
+      times_helped--;
+    }
   }
-
+  printf("the student %d ended his program\n",i); 
   pthread_exit(0);
 }
--- main.c
+++ main.c
@@ -3,27 +3,34 @@
 
 int main (){
   srand(time(NULL));  
-  //num_students = random_number(3,40);
-  num_students = 3;
-
+  num_students = random_number(3,40);
   int num = num_students;
 
-  printf("%d\n",num);
-  
+  queue_max_size = num/2;
+  queue = (int*)malloc(sizeof(int)*queue_max_size);
+
+  pthread_mutex_init(&queue_mutex,NULL);
+  pthread_mutex_init(&assistent_state_mutex,NULL);
+
+  int count;
+  for(count = 0; count < num; count++){
+    sem_init(&turn_sem[count],0,1);
+  }
+
   sem_init(&assistent_sem,0,1);
   sem_init(&student_sem,0,1);
-  sem_init(&turn_sem,0,1);
 
   pthread_t assistent_id;
   pthread_attr_t assistent_attr;
   pthread_attr_init(&assistent_attr);
   pthread_create(&assistent_id,&assistent_attr,assistent,NULL);
  
-  int id[9] = {1,2,3,4,5,6,7,8,9};
+  int id[num];
   pthread_t student_id[num];
   pthread_attr_t student_attr[num];
   int i;
   for(i = 0; i < num; i++){
+    id[i]=i+1;
     pthread_attr_init(&student_attr[i]);
     pthread_create(&student_id[i],&student_attr[i],student, &id[i]);
   }
@@ -31,8 +38,18 @@ int main (){
   for(i = 0; i < num; i++){
     pthread_join(student_id[i],NULL);
   }
+  pthread_cancel(assistent_id); 
   pthread_join(assistent_id,NULL);
   printf("\n");
-
+  
+/*  sem_destroy(&assistent_sem);
+  sem_destroy(&student_sem);
+  for(i = 0; i < num; i++){
+    sem_destroy(&turn_sem[i]);
+  }
+  pthread_mutex_destroy(&queue_mutex);
+  free(queue);
+  free(turn_sem);
+*/
   return 0;
 }
